fb5831a53c389038154a7575f4530d9dd515d985
Frederico Bitencourt <fredericofbh@gmail.com>
2018-11-09 19:21:15 -0200

    Ajustes no endpoint /categorias/{id} Tratamento de excecoes com Handler

---
+++ src/main/java/com/frederico/cursomc/resources/exceptions/StandardError.java
@@ -0,0 +1,25 @@
+package com.frederico.cursomc.resources.exceptions;
+
+import java.io.Serializable;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class StandardError implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Integer status;
+	private String msg;
+	private Long timeStamp;
+	
+	public StandardError(Integer status, String msg, Long timeStamp) {
+		super();
+		this.status = status;
+		this.msg = msg;
+		this.timeStamp = timeStamp;
+	}
+
+}
