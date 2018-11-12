3f84db6c8a8e185dfbd42d62593319e92425f753
Frederico Bitencourt <fredericofbh@gmail.com>
2018-11-12 13:52:04 -0200

    Adicionado Cliente, TipoCliente, telefones e enderecos

--- src/main/java/com/frederico/cursomc/CursomcApplication.java
+++ src/main/java/com/frederico/cursomc/CursomcApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.frederico.cursomc.domain.Categoria;
 import com.frederico.cursomc.domain.Cidade;
+import com.frederico.cursomc.domain.Cliente;
+import com.frederico.cursomc.domain.Endereco;
 import com.frederico.cursomc.domain.Estado;
 import com.frederico.cursomc.domain.Produto;
+import com.frederico.cursomc.domain.enums.TipoCliente;
 import com.frederico.cursomc.repositories.CategoriaRepository;
 import com.frederico.cursomc.repositories.CidadeRepository;
+import com.frederico.cursomc.repositories.ClienteRepository;
+import com.frederico.cursomc.repositories.EnderecoRepository;
 import com.frederico.cursomc.repositories.EstadoRepository;
 import com.frederico.cursomc.repositories.ProdutoRepository;
 
@@ -30,6 +35,12 @@ public class CursomcApplication implements CommandLineRunner {
 	
 	@Autowired
 	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -67,6 +78,18 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "34844267051", TipoCliente.PESSOAFISICA);
+		
+		cli1.getTelefones().addAll(Arrays.asList("33326584", "956234185"));
+		
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "30226845", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "20665841", cli1, c2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
 		
 	}
 }
---
+++ src/main/java/com/frederico/cursomc/domain/Cliente.java
@@ -0,0 +1,70 @@
+package com.frederico.cursomc.domain;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.CollectionTable;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.frederico.cursomc.domain.enums.TipoCliente;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Cliente implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@EqualsAndHashCode.Include
+	private Integer id;
+	
+	private String nome;
+	
+	private String email;
+	
+	private String cpfOuCnpj;
+	
+	private Integer tipo;
+	
+	@OneToMany(mappedBy = "cliente")
+	private List<Endereco> enderecos = new ArrayList<Endereco>();
+	
+	@ElementCollection
+	@CollectionTable(name = "telefone")
+	private Set<String> telefones = new HashSet<String>();
+
+	public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.cpfOuCnpj = cpfOuCnpj;
+		this.tipo = tipo.getCod();
+	}
+	
+	public TipoCliente getTipo() {
+		return TipoCliente.toEnum(tipo);
+	}
+	
+	public void setTipo(TipoCliente tipo) {
+		this.tipo = tipo.getCod();
+	}
+
+	
+}
